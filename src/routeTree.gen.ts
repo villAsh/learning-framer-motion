6967ad781b71c0240e11b77856f739f1c69886f9
villAsh <chavhanvilas02@gmail.com>
2025-04-29 19:06:53 +0530

    feat: learned about basics of framer motion

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,12 +12,19 @@
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as IndexImport } from './routes/index'
+import { Route as ExampleOneIndexImport } from './routes/exampleOne/index'
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const ExampleOneIndexRoute = ExampleOneIndexImport.update({
+  id: '/exampleOne/',
+  path: '/exampleOne/',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -32,6 +39,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/exampleOne/': {
+      id: '/exampleOne/'
+      path: '/exampleOne'
+      fullPath: '/exampleOne'
+      preLoaderRoute: typeof ExampleOneIndexImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +53,37 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/exampleOne': typeof ExampleOneIndexRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/exampleOne': typeof ExampleOneIndexRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/exampleOne/': typeof ExampleOneIndexRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/exampleOne'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/exampleOne'
+  id: '__root__' | '/' | '/exampleOne/'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  ExampleOneIndexRoute: typeof ExampleOneIndexRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  ExampleOneIndexRoute: ExampleOneIndexRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +96,15 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/exampleOne/"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/exampleOne/": {
+      "filePath": "exampleOne/index.tsx"
     }
   }
 }
